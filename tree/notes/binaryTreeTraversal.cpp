fc5300d995c2ee8ad4631da21293a8b4812ef078
whisper <64951456+jy19950924@users.noreply.github.com>
2022-10-11 10:54:21 +0800

    Update binaryTreeTraversal.cpp

--- tree/notes/binaryTreeTraversal.cpp
+++ tree/notes/binaryTreeTraversal.cpp
@@ -1,60 +1,41 @@
 //
 // Created by dell on 9/20/2022.
 //
-#include "../../public.h"
+#include "../tree.h"
 #include <stack>
-
-using std::stack;
+#include <vector>
+#include <iostream>
 
 // 先序遍历。主要是非递归版本
-class Solution {
-public:
-    vector<int> inorderTraversal(TreeNode *root) {
-        stack < TreeNode * > stk;
-        vector<int> res;
-        if (root == nullptr) return res;
-        stk.push(root);
-        while (!stk.empty()) {
-            TreeNode *cur = stk.top();
-            stk.pop();
-            res.push_back(cur->val);
-            if (cur->right != nullptr) {
-                stk.push(cur->right);
-            }
-            if (cur->left != nullptr) {
-                stk.push(cur->left);
-            }
+void inorderTraversal(TreeNode *root)
+{
+    using namespace std;
+    stack<TreeNode *> stk;
+    vector<int> res;
+    if (root == nullptr)
+        return;
+    stk.push(root);
+    while (!stk.empty())
+    {
+        TreeNode *cur = stk.top();
+        stk.pop();
+        cout << cur->val << ' ';
+        if (cur->right != nullptr)
+        {
+            stk.push(cur->right);
         }
-        return res;
+        if (cur->left != nullptr)
+        {
+            stk.push(cur->left);
+        }
     }
-};
+}
 
-/**
- * 1
- * 2 3
- * 4 -1 5 -1
- * -1 6
- * @return
- */
-int main() {
-    TreeNode *head = TreeNode(1);
-    TreeNode *tail = head;
-//    first floor
-    TreeNode *node2 = new TreeNode(2);
-    TreeNode *node3 = new TreeNode(3);
-    head->left = node2;
-    head->right = node3;
-//    second floor
-    TreeNode *node4 = new TreeNode(4);
-    TreeNode *node5 = new TreeNode(5);
-    node2->left = node4;
-    node2->right = nullptr;
-    node3->left = node5;
-    node3->right = nullptr;
-//    third floor
-    TreeNode *node6 = new TreeNode(6);
-    node4->left = nullptr;
-    node4->right = node6;
+int main()
+{
+    int data[] = {0, 2, 4, 6, 3, 5, 77, 8, 8, 9};
+    int len = sizeof(data) / sizeof(int);
 
-
+    Tree tree1 = Tree(data, len);
+    inorderTraversal(tree1.root); //2 4 3 8 9 5 6 77 8
 }
