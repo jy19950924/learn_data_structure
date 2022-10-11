daaf34ef24302cfe3ac6a0995a2606c02eb3d651
whisper <64951456+jy19950924@users.noreply.github.com>
2022-10-11 19:37:15 +0800

    Create postorderTraversal.cpp

---
+++ tree/notes/postorderTraversal.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include "../tree.h"
+#include <stack>
+
+void postorderTraversal(TreeNode *root)
+{
+    using namespace std;
+
+    stack<TreeNode *> helpStack;
+    TreeNode *prev = nullptr, *cur = root;
+
+    while (!helpStack.empty() || cur != nullptr)
+    {
+        if (cur != nullptr)
+        {
+            helpStack.push(cur);
+            cur = cur->left;
+        }
+        else
+        {
+            TreeNode *parentNode = helpStack.top();
+            if (parentNode->right == nullptr || parentNode->right == prev)
+            {
+                cout << parentNode->val << ' ';
+                helpStack.pop();
+                cur = nullptr;
+            }
+            else
+            {
+                cur = parentNode->right;
+            }
+            prev = parentNode;
+        }
+    }
+}
+
+int main()
+{
+    using namespace std;
+    int data[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int len = sizeof(data) / sizeof(int);
+
+    Tree tree = Tree(data, len);
+    cout << "postorder traversal: ";
+    postorderTraversal(tree.root);
+
+    return 0;
+}
