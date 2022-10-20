2c9c2cfd52677e15e2863146370e55ec277b092a
whisper <64951456+jy19950924@users.noreply.github.com>
2022-10-20 11:33:23 +0800

    Update drawBinaryTree.cpp

--- tree/drawBinaryTree.cpp
+++ tree/drawBinaryTree.cpp
@@ -15,90 +15,84 @@ struct Floor {
         : lines(v_lines), width(i_width), height(i_height), dist(i_dist){};
 };
 
-Floor draw(TreeNode *root)
+Floor *draw(TreeNode *root)
 {
+    string s_val = to_string(root->val);
+    int s_nval = s_val.size();
     if (root->left == nullptr && root->right == nullptr) {
-        string s_val = to_string(root->val);
-        deque<string> lines;
-        lines.push_back(s_val);
-        int width = s_val.size();
-        Floor n_floor = Floor(lines, width, 1, width / 2);
-        return n_floor;
-    } else if (root->lseft == nullptr) {
-        Floor right = draw(root->right);
 
-        string s_val = to_string(root->val);
-        string s_nodeFloor = s_val + string("_", right.dist) + string("_", right.width - right.dist);
-        // 计算s_flagFloor 时-1是减去\\的值
-        string s_flagFloor = string(" ", s_val.size() + right.dist) + "\\" + string(" ", right.width - right.dist - 1);
-        for (int i = 0; i < right.lines.size(); i++) {
-            right.lines[i] = string(" ", s_val.size()) + right.lines[i];
+        deque<string> lines = {s_val};
+        int width = s_val.size();
+        Floor *n_floor = new Floor(lines, width, 1, width / 2);
+        return n_floor;
+
+    } else if (root->left == nullptr) {
+        Floor *right = draw(root->right);
+
+        string s_nodeFloor = s_val + string(right->dist, '_') + string(right->width - right->dist, '_');
+        string s_flagFloor = string(s_nval + right->dist, ' ') + "\\" + string(right->width - right->dist - 1, ' ');
+        for (int i = 0; i < right->lines.size(); i++) {
+            right->lines[i] = string(s_val.size(), ' ') + right->lines[i];
         }
-        right.width += s_val.size();
-        right.height += 2;
-        right.dist = s_val.size() / 2;
-        right.lines.push_front(s_flagFloor);
-        right.lines.push_front(s_nodeFloor);
+        right->width += s_nval;
+        right->height += 2;
+        right->dist = s_nval / 2;
+        right->lines.push_front(s_flagFloor);
+        right->lines.push_front(s_nodeFloor);
         return right;
     } else if (root->right == nullptr) {
-        Floor left = draw(root->left);
+        Floor *left = draw(root->left);
 
-        string s_val = to_string(root->val);
-        string s_nodeFloor = string(" ", left.dist + 1) + string("_", left.width - left.dist - 1) + s_val;
-        string s_flagFloor = string(" ", left.dist) + "/" + string(" ", left.width - left.dist - 1 + s_val.size());
-        for (int i = 0; i < left.lines.size(); i++) {
-            left.lines[i] = left.lines[i] + string(" ", s_val.size());
+        string s_nodeFloor = string(left->dist + 1, ' ') + string(left->width - left->dist - 1, '_') + s_val;
+        string s_flagFloor = string(left->dist, ' ') + "/" + string(left->width - left->dist - 1 + s_nval, ' ');
+        for (int i = 0; i < left->lines.size(); i++) {
+            left->lines[i] = left->lines[i] + string(s_nval, ' ');
         }
-        left.width = left.width + s_val.size();
-        left.height += 2;
-        left.dist = left.dist + s_val.size() / 2;
-        left.lines.push_front(s_flagFloor);
-        left.lines.push_front(s_nodeFloor);
+        left->dist = left->width + s_nval / 2;
+        left->width = left->width + s_nval;
+        left->height += 2;
+
+        left->lines.push_front(s_flagFloor);
+        left->lines.push_front(s_nodeFloor);
         return left;
     }
-    Floor left = draw(root->left);
-    Floor right = draw(root->right);
-    string s_val = to_string(root->val);
-    string s_nodeFloor = string(" ", left.dist + 1) + string("_", left.width - left.dist - 1) + s_val +
-                         string("_", right.dist) + string(" ", right.width - right.dist);
-    string s_flagFloor = string(" ", left.dist) + "/" + string(" ", left.width - left.dist - 1) +
-                         string(" ", s_val.size()) + string(" ", right.dist) + "\\" +
-                         string(" ", right.width - right.dist - 1);
+    Floor *left = draw(root->left);
+    Floor *right = draw(root->right);
+    string s_nodeFloor = string(left->dist + 1, ' ') + string(left->width - left->dist - 1, '_') + s_val +
+                         string(right->dist, '_') + string(right->width - right->dist, ' ');
+    string s_flagFloor = string(left->dist, ' ') + "/" + string(left->width - left->dist - 1, ' ') +
+                         string(s_nval, ' ') + string(right->dist, ' ') + "\\" +
+                         string(right->width - right->dist - 1, ' ');
     int index = 0;
-    while (index < left.lines.size()) {
-        if (index < right.lines.size()) {
-            left.lines[index] = left.lines[index] + string(" ", s_val.size()) + right.lines[index];
-        } else {
-            left.lines[index] += string(" ", s_val.size()) + string(" ", right.width);
-        }
+    while (index < left->lines.size()) {
+        if (index < right->lines.size())
+            left->lines[index] = left->lines[index] + string(s_nval, ' ') + right->lines[index];
+        else
+            left->lines[index] += string(s_nval, ' ') + string(right->width, ' ');
         index++;
     }
-    string s_spaceLeft = string(" ", left.width);
-    while (index < right.lines.size()) {
-        left.lines.push_back(s_spaceLeft + string(" ", s_val.size()) + right.lines[index]);
+    string s_spaceLeft = string(left->width, ' ');
+    while (index < right->lines.size()) {
+        left->lines.push_back(s_spaceLeft + string(s_nval, ' ') + right->lines[index]);
         index++;
     }
-    left.width = left.width + s_val.size() + right.width;
-    left.height = left.height > right.height ? (left.height + 2) : (right.height + 2);
-    left.dist = left.width + (s_val.size()) / 2;
-    left.lines.push_front(s_flagFloor);
-    left.lines.push_front(s_nodeFloor);
+    left->dist = left->width + (s_nval) / 2;
+    left->width = left->width + s_nval + right->width;
+    left->height = left->height > right->height ? (left->height + 2) : (right->height + 2);
+
+    left->lines.push_front(s_flagFloor);
+    left->lines.push_front(s_nodeFloor);
     return left;
 }
 
 int main()
 {
-    int data[] = {1, 2, 3, 4};
+    int data[] = {1, 2, 3, 4, 5, 6, 7, 8, 1, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 8, 7, 6, 4, 3};
     int len = sizeof(data) / sizeof(int);
 
     Tree *ptree = new Tree(data, len);
-
-    deque<string> que = {"nice"};
-
-    Floor floor = draw(ptree->root);
-
-    for (int i = 0; i < floor.lines.size(); i++) {
-        cout << floor.lines[i] << endl;
+    Floor *floor = draw(ptree->root);
+    for (int i = 0; i < floor->lines.size(); i++) {
+        cout << floor->lines[i] << endl;
     }
     return 0;
-}
