0b207782ab969575bac0e19696c907f71424f80d
Whisper <jylovehtz@gmail.com>
2022-12-06 22:58:33 +0800

    add trie Tree

--- tree/trieTree.h
+++ tree/trieTree.h
@@ -10,26 +10,26 @@ using std::string;
 const int MAX_NEXT_SET = 26;
 
 namespace TrieTree {
-struct Node {
-  int pass;
-  int end;
-  Node *nexts[MAX_NEXT_SET] = {nullptr};
-  Node() : pass(0), end(0) {}
-};
-class TrieTree {
-  Node *_root;
+  struct Node {
+    int pass;
+    int end;
+    Node* nexts[MAX_NEXT_SET] = { nullptr };
+    Node() : pass(0), end(0) {}
+  };
+  class TrieTree {
+    Node* _root;
 
-public:
-  TrieTree();
+  public:
+    TrieTree();
 
-  void Insert(string s);
+    void Insert(string s);
 
-  void Delete();
+    void Delete(string s);
 
-  int Count(string s);
-  int CountPrefixed(string s);
-};
+    int Count(string s);
 
+    int CountPrefixed(string s);
+  };
 } // namespace TrieTree
 
 #endif
