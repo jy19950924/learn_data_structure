901fa6a77af52d16de3271b9fd6f2f354a479fac
whisper <64951456+jy19950924@users.noreply.github.com>
2022-12-06 17:39:55 +0800

    Update trieTree.cc

--- tree/trieTree.cc
+++ tree/trieTree.cc
@@ -1,1 +1,19 @@
 
+#include "trieTree.h"
+
+TrieTree::TrieTree::TrieTree() { this->_root = new Node(); }
+
+void TrieTree::TrieTree::Insert(string s) {
+  Node *cur = this->_root;
+  const char *p = s.c_str();
+  int s_len = strlen(p);
+  for (int i = 0; i < s_len; i++) {
+    cur->pass++;
+    int index = p[i]-'a';
+    if(!cur->nexts[index]){
+      cur->nexts[index] = new Node();
+    }
+    cur = cur->nexts[index];
+  }
+  cur->end ++;
+}
