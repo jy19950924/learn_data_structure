776a44b08822e1636e8ee9f19878697a949f4370
Whisper <jylovehtz@gmail.com>
2023-05-07 16:00:36 +0800

    add binarySearch method 2

--- Search/binarySearch.cc
+++ Search/binarySearch.cc
@@ -13,7 +13,8 @@ public:
         int left = 0, right = len - 1;
         int medium;
 
-        while (left < len && right < len && left < right) {
+        // 不需要判断left和right上下限,因为left<right,其中left只增加,而right只减;
+        while (left < right) {
             medium = left + (right - left) / 2;
             if (nums[medium] < target) {
                 left = medium + 1;
@@ -26,5 +27,20 @@ public:
         return -1;
     }
 
+    int binarySearch_2(vector<int>& nums, int target) {
+        int len = nums.size();
+        int left = 0, right = len - 1, medium;
+
+        while (left < right) {
+            medium = left + (right - left) / 2;
+            if (nums[medium] < target) {
+                left = medium + 1;
+            } else {
+                right = medium;
+            }
+        }
+        return (nums[left] == target) ? (left) : (-1);
+    }
 }
+
 
